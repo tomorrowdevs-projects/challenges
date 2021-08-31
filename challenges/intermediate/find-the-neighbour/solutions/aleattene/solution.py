1a4a81823b5218ba8be17fda67d4f928dce5ebdc
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-08-31 17:39:46 +0200

    refactor: optimized the code after the feedbacks received

--- challenges/intermediate/find-the-neighbour/solutions/aleattene/solution.py
+++ challenges/intermediate/find-the-neighbour/solutions/aleattene/solution.py
@@ -4,37 +4,27 @@ Python solution for challenge: "Find the neighbour"
 To start the tests, type from CLI: python tests.py
 """
 
+import re
+import math
+
 
 def find_neighbour(cube, query):
-    # Data structures
-    directions = {"left": -1, "right": +1, "upstairs": -3, "downstairs": +3}
-    names_list = []
-    # Creation of the list of names present in the cube
-    for line in cube.split("|"):
-        # Remove spaces at the beginning and at the end of the string
-        line = line.strip()
-        if line != "":
-            if "A" <= line[0] <= "Z":
-                names_list.append(line)
+    # List containing the names (currently works only with the English alphabet)
+    names_list = re.findall("[a-zA-Z]+", cube)
+    # Length of the row (or column) of the cube
+    row_len = int(math.sqrt(len(names_list)))
+    # Data structure to manage the four search directions
+    directions = {"left": -1, "right": +1, "upstairs": -row_len, "downstairs": +row_len}
     # Name and direction (taken from the input string)
-    query_name, query_direction = query.split(":")[0], query.split(" ")[2]
+    query_name, query_direction = re.findall("[a-zA-Z]+", query)[0:3:2]
     # Index of the query_name in the names_list
     index_name = names_list.index(query_name)
     # Index of the neighbor in the names_list
     index_neighbor = index_name + directions[query_direction]
-    # Query_direction is equal to left or right
-    if query_direction in ['left', 'right']:
-        # Check if the index_neighbor is correct (in the cube_lst and in the same row)
-        if 0 <= index_neighbor <= len(names_list) - 1 and index_name // 3 == index_neighbor // 3:
-            # Return the name of the neighbor
-            return names_list[index_neighbor]
-        else:
-            # No neighbor
-            return "nobody"
-    # Query_direction is equal to upstairs or downstairs
-    elif 0 <= index_neighbor <= len(names_list) - 1:
-        # Return the name of the neighbor
-        return names_list[index_neighbor]
-    else:
-        # No neighbor
+    # Check if the index_neighbor is not correct (out of the extremes of the cube or not in the same row)
+    if (query_direction in ['left', 'right'] and (index_name // row_len != index_neighbor // row_len)) \
+            or not(0 <= index_neighbor <= len(names_list) - 1):
         return "nobody"
+    # The index of the neighbor is correct and therefore, it is possible to return the real name.
+    else:
+        return names_list[index_neighbor]
