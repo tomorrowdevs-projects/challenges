1a5b146fbd213d5fffaaa09ed5e36fd8cfc33797
aldotele <aldo.telese@hotmail.it>
2021-09-05 21:01:51 +0200

    fix: added argument to vaporcode function

--- challenges/easy/vaporcode/solutions/aldotele/main.hs
+++ challenges/easy/vaporcode/solutions/aldotele/main.hs
@@ -9,10 +9,12 @@ replace xs c s = foldr go [] xs
     where go x acc = if x == c  then s ++ acc 
                                 else [x] ++ acc 
                                 
-main = print (vaporcode_string)
+vaporcode :: String -> String
+vaporcode s = vaporcode_string
     where
-        input_string = " Hello Folks. This is Haskell! " -- try changing this
-        string_uppercase = uppercase input_string
+        string_uppercase = uppercase s
         string_nospace = replace string_uppercase ' ' ""
         string_onespaced = intersperse ' ' string_nospace
         vaporcode_string = intersperse ' ' string_onespaced  -- adding 2nd space
+
+main = print(vaporcode " Hello   World! This is haskell   ")
