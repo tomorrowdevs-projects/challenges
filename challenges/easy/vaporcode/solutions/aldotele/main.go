e490be8a7836be884af6e28ec5dc7fd856a23ee7
aldotele <aldo.telese@hotmail.it>
2021-09-07 12:10:30 +0200

    refactor: more functional approach

--- challenges/easy/vaporcode/solutions/aldotele/main.go
+++ challenges/easy/vaporcode/solutions/aldotele/main.go
@@ -1,26 +1,35 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+  "bufio"
+  "fmt"
+  "os"
+  "strings"
 )
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("enter word/sentence: ")
-	if scanner.Scan() {
-		var w string = scanner.Text()
-		// removing spaces and converting to uppercase
-		word_stripped := strings.ReplaceAll(w, " ", "")
-		word_upper := strings.ToUpper(word_stripped)
+func space(s string, spacing string) string {
+  // adding a (changeable) spacing between each character of a string
+  spaced := ""
+  iterable := []rune(strings.ReplaceAll(s, " ", ""))
+  for i := 0; i < len(iterable); i++ {
+    spaced += string(iterable[i]) + spacing
+  }
+  return spaced
+} 
 
-		word_iterable := []rune(word_upper)
-		//Iterate
-		for i := 0; i < len(word_iterable); i++ {
-			fmt.Print(string(word_iterable[i]) + "  ")
-		}
-	}
+func vaporcode(s string) string {
+  // returns an uppercase, double-spaced string
+  return strings.ToUpper(space(s, "  "))
+}
+
+func main(){
+  // asking input to the user
+  scanner := bufio.NewScanner(os.Stdin)
+  fmt.Print("enter word/sentence: ")
+  if scanner.Scan() {
+    // storing and printing the vaporwave
+    vaporwave := vaporcode(scanner.Text())
+    fmt.Print(vaporwave)
+  } 
 }
 
