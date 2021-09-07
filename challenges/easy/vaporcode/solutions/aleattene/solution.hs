7a37b334ba241435a48fed95bc8a05c1837d4fa8
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-09-07 12:48:54 +0200

    refactor: updated code after feedbacks received during the code review

--- challenges/easy/vaporcode/solutions/aleattene/solution.hs
+++ challenges/easy/vaporcode/solutions/aleattene/solution.hs
@@ -4,11 +4,9 @@ module Vaporcode where
 import Data.Char
 
 vaporcode :: String -> String
+vaporcode str = (reverse . drop 2 . reverse)(map toUpper(concatMap(\x -> if x /= ' ' then [x] ++ "  " else "") str))
 
--- Removing the last two spaces of the string
-removeLastTwoSpaces = reverse . drop 2 . reverse
-
--- Adding two spaces for each character of the string (other than the space itself)
--- Capitalization of the string
--- Deleting two spaces at the end of the string
-vaporcode str = removeLastTwoSpaces(map toUpper(concatMap(\x -> if x /= ' ' then [x] ++ "  " else "") str))
+-- Resolution algorithm
+--    Adding two spaces for each character of the string (other than the space itself)
+--    Capitalization of the string
+--    Deleting two spaces at the end of the string
