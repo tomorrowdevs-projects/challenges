a3fdf69229c0cf030d5ecc658d9823dba5aa564f
aldotele <aldo.telese@hotmail.it>
2021-09-10 10:58:37 +0200

    refactor: func and indent change

--- challenges/code-golf/fizzbuzz/solutions/aldotele/main.py
+++ challenges/code-golf/fizzbuzz/solutions/aldotele/main.py
@@ -1,3 +1,3 @@
+f=lambda x:i%x==0
 for i in range(1,101):
-    c=[i%3==0,i%5==0]
-    print("FizzBuzz" if all(c) else "Fizz" if c[0] else "Buzz" if c[1] else i)
+ print("FizzBuzz" if f(15) else "Fizz" if f(3) else "Buzz" if f(5) else i)
