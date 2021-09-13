16fa085b12adb9da6c553ff8e08df77d731b3dd5
aldotele <aldo.telese@hotmail.it>
2021-09-13 09:44:52 +0200

    fix: now using infinite loop instead of recursion for inputs

--- challenges/easy/who-likes-it/solutions/aldotele/main.go
+++ challenges/easy/who-likes-it/solutions/aldotele/main.go
@@ -1,35 +1,50 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "bufio"
+  "os"
+  "strings"
+)
 
 func main() {
-  fmt.Println("Enter people below (blank to quit)")
-  likes := enterLikes([]string{}, nil)
+  likes := ask_likes()
   fmt.Printf(makeSentence(likes))
 }
 
-func enterLikes(x []string, err error) []string {
-    if err != nil {
-        return x
+func ask_likes() []string {
+  likes := []string{}
+  scanner := bufio.NewScanner(os.Stdin)
+  fmt.Println("Enter person below (blank to quit)")
+  // building an infinite loop until a blank is entered
+  for true {
+    if scanner.Scan() {
+      p := scanner.Text()
+      // the following makes sure only the name is extracted
+      name := strings.Split(p, " ")[0]
+      if name != "" {
+        likes = append(likes, name)
+      } else {
+        break
+      }
     }
-    var person string
-    n, err := fmt.Scanf("%s", &person)
-    if n == 1 {
-        x = append(x, person)
-    }
-    return enterLikes(x, err)
+    fmt.Println("Enter person below (blank to quit)")
+  }
+  return likes
 }
 
 func makeSentence(likes []string) string {
-  if len(likes) == 1 {
+  switch likesCount := len(likes); {
+  case likesCount == 1:
     return fmt.Sprintf("%s like this", likes[0])
-  } else if len(likes) == 2 {
+  case likesCount == 2:
     return fmt.Sprintf("%s and %s like this", likes[0], likes[1])
-  } else if len(likes) == 3 {
+  case likesCount == 3:
     return fmt.Sprintf("%s, %s and %s like this", likes[0], likes[1], likes[2])
-  } else if len(likes) > 3 {
+  case likesCount > 3:
     var rem int = len(likes) - 2
     return fmt.Sprintf("%s, %s and %d others like this", likes[0], likes[1], rem)
   }
-  return fmt.Sprintf("no one likes this")
+  return "no one likes this"
 }
+
