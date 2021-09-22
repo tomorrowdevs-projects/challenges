395407d3194b57ecf10ea8d688cc001e2d6ca1d9
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-09-22 18:02:21 +0200

    style: modified some comments

--- challenges/easy/stack/solutions/aleattene/main.clj
+++ challenges/easy/stack/solutions/aleattene/main.clj
@@ -5,12 +5,12 @@
 
 (defn setStack [] (def .stack ()))
 
-;; PUSH method =>  Adds an item to the top of the stack
+;; PUSH method =>  Adds an item to the top of the stack (LIFO)
 (defn push [stack item] (def .stack (conj stack item)))
 
-;; POP method => Removes the top item from the stack => Built-in Clojure function
+;; POP method => Removes the top item from the stack (LIFO) => Built-in Clojure function
 
-;; PEEK method => Returns a copy of the top item in the stack -> Built-in Clojure function
+;; PEEK method => Returns a copy of the top item in the stack => Built-in Clojure function
 
 ;; ISEMPTY method => Returns a boolean indicating whether or not the stack is empty
 (defn isEmpty [stack] (empty? stack))
@@ -24,17 +24,17 @@
 (print "\nINITIAL STACK: ")
 (println .stack)
 ;; PUSH test
-(println "\nPUSH (each time adds an item to the top of the stack): ")
-(push .stack 3)
+(println "\nPUSH (each time adds an item to the top of the stack (LIFO)): ")
+(push .stack 1)
 (println .stack)
 (push .stack 2)
 (println .stack)
-(push .stack 1)
+(push .stack 3)
 (println .stack)
-(push .stack 0)
+(push .stack 4)
 (println .stack)
 ;; POP test
-(println "\nPOP (each time removes the top item from the stack): ")
+(println "\nPOP (each time removes the top item from the stack (LIFO)): ")
 (def .stack (pop .stack))
 (println .stack)
 (def .stack (pop .stack))
@@ -49,7 +49,7 @@
 (print "\nNUMBER of ITEMS in the stack: ")
 (println (size .stack))
 ;; POP test
-(println "\nPOP (each time removes the top item from the stack): ")
+(println "\nPOP (each time removes the top item from the stack (LIFO)): ")
 (def .stack (pop .stack))
 (println .stack)
 (def .stack (pop .stack))
