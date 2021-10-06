cf114a938c9c33b302ec112f872f6cbc0adf7bba
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-06 14:46:23 +0200

    feat: added js (node.js) solution for challenge divisors-of-a-number

---
+++ challenges/easy/divisor-of-a-number/solutions/aleattene/solution.js
@@ -0,0 +1,48 @@
+/*
+JS solution for challenge: "Divisors of a Number"
+To test the solution, type from CLI: node solution.js
+*/
+
+function divisors(number) {
+    let divisor = 1;
+    let counter = 0;
+    while (divisor <= Math.floor(number/divisor)) {
+        number % divisor == 0 ? Math.floor(number/divisor) > divisor ? counter += 2 : counter += 1 : counter = counter
+        divisor += 1;
+    }
+    return counter
+}
+
+
+// TEST CASES
+console.log("\nExpected sequence: 1 2 2 3 2 2 6 8 8 6 8 10 4 2:")
+sequence = ""
+console.log("     divisors(1) = " + divisors(1) + " | | | | | | | | | | || | |")      // expected 1 (1)
+sequence += divisors(1)
+console.log("       divisors(2) = " + divisors(2) + " | | | | | | | | | || | |")      // expected 2 (1,2)
+sequence += divisors(2)
+console.log("         divisors(3) = " + divisors(3) + " | | | | | | | | || | |")      // expected 2 (1,3)
+sequence += divisors(3)
+console.log("           divisors(4) = " + divisors(4) + " | | | | | | | || | |")      // expected 3 (1,2,4)
+sequence += divisors(4)
+console.log("             divisors(5) = " + divisors(5) + " | | | | | | || | |")      // expected 2 (1,5)
+sequence += divisors(5)
+console.log("               divisors(7) = " + divisors(7) + " | | | | | || | |")      // expected 2 (1,7)
+sequence += divisors(7)
+console.log("                divisors(12) = " + divisors(12) + " | | | | || | |")     // expected 6 (1,2,3,4,6,12)
+sequence += divisors(12)
+console.log("                  divisors(24) = " + divisors(24) + " | | | || | |")     // expected 8 (1,2,3,4,6,8,12,24)
+sequence += divisors(24)
+console.log("                    divisors(30) = " + divisors(30) + " | | || | |")     // expected 8 (1,2,3,5,6,10,15,30)
+sequence += divisors(30)
+console.log("                      divisors(99) = " + divisors(99) +  " | || | |")    // expected 6 (1,3,9,11,33,99)
+sequence += divisors(99)
+console.log("                       divisors(130) = " + divisors(130) +  " || | |")   // expected 8 (1,2,5,10,13,26,65,130)
+sequence += divisors(130)
+console.log("                        divisors(1875) = " + divisors(1875) +  " | |")   // expected 10 (1,3,5,15,25,75,125,375,625,1875)
+sequence += divisors(1875)
+console.log("                           divisors(4997) = " + divisors(4997) + " |")   // expected 4 (1,19,263,4997)
+sequence += divisors(4997)
+console.log("                            divisors(10589) = " + divisors(10589))       // expected 2 (1,10589)
+sequence += divisors(10589)
+sequence === '122322688681042' ? console.log('\nCongratulations, all tests have been passed.') : console.log('\nAttention, tests failed.')
