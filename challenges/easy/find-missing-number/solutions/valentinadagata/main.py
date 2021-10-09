39d2a9a10dfae4f3adc3eb86f8bce9b632dc2f2d
Edera <edera.marketinglab@gmail.com>
2021-10-09 15:26:28 +0200

    fix: sort the list and take the first and the last item to iterate only 1 time (instead of using max and min)

--- challenges/easy/find-missing-number/solutions/valentinadagata/main.py
+++ challenges/easy/find-missing-number/solutions/valentinadagata/main.py
@@ -1,7 +1,9 @@
 def find_missing_number(l):
+    # sort the list to get the min and the max taking the first and the last item
+    l.sort()
     # total list = sum all the numbers between the min and the max of the list
     # difference between the total list and the list will be the missing number
-    n = sum(range(min(l), max(l) + 1)) - sum(l)
+    n = sum(range(l[0], l[len(l)-1] + 1)) - sum(l)
     return n
 
 
