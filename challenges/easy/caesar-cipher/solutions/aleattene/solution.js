141b8bf3fd772946b78432bdd8bd50757f8bcf12
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-22 16:14:50 +0200

    refactor: improved the solution based on the feedback received (Regex and for loop removed)

--- challenges/easy/caesar-cipher/solutions/aleattene/solution.js
+++ challenges/easy/caesar-cipher/solutions/aleattene/solution.js
@@ -5,53 +5,26 @@ To test the solution, type from CLI: npm test (required node.js and jest framewo
 
 
 function encode(originalMessage, shift) {
-    let ALPHABET = 26;
-    // Ascii Code Lower and Upper Chars
-    let LOWER_MIN = 65, LOWER_MAX = 90, UPPER_MIN = 97, UPPER_MAX = 122;
-    // Shift normalization (based on the number of characters of the alphabet)
-    shift %= ALPHABET;
-    // Blank encrypted message
-    let encryptedMessage = "";
-    // Start encryption
-    for (let i = 0; i < originalMessage.length; i++) {
-        // Single char of the message
-        let char = originalMessage[i];
-        // Uppercase char to encrypt
-        if ((/[A-Z]/).test(char)) {
-            encryptedMessage += charEncryption(char, shift, LOWER_MIN, LOWER_MAX, ALPHABET);
-        }
+    // This function returns the message encrypted according to the Caesar cipher
+    let CHARS = 26;
+    return originalMessage.split("").map(char => {
         // Lowercase char to encrypt
-        else if ((/[a-z]/).test(char)) {
-            encryptedMessage += charEncryption(char, shift, UPPER_MIN, UPPER_MAX, ALPHABET);
+        if (char >= "a" && char <= "z") {
+            return String.fromCharCode(97 + (((char.charCodeAt(0) - 97) + (CHARS + (shift % CHARS))) % CHARS));
+        }
+        // Uppercase char to encrypt
+        else if (char >= "A" && char <= "Z") {
+            return String.fromCharCode(65 + (((char.charCodeAt(0) - 65) + (CHARS + (shift % CHARS))) % CHARS));
         }
         // Special char (not to be encrypted)
-        else encryptedMessage += char;
-    }
-    // Returns the message encrypted according to the Caesar cipher
-    return encryptedMessage;
+        else return char;
+    }).join("");
 }
 
 
 function decode(encryptedMessage, shift) {
-    // Returns the decrypted (original) message according to the Caesar cipher
+    // Thi function returns the decrypted (original) message according to the Caesar cipher
     return(encode(encryptedMessage, -shift));
-}
-
-
-function charEncryption(char, shift, limit_min, limit_max, alphabet) {
-        // Ascii code of the shifted char
-        let shiftedCharAsciiCode = char.charCodeAt(0) + shift;
-        // By default the resulting ASCII code is assumed to be within the allowed range
-        let asciiCodeRepositioning = 0
-        // Checks if the resulting ASCII code is within the allowed range
-        if (shiftedCharAsciiCode < limit_min) {
-            asciiCodeRepositioning = alphabet;
-        }
-        else if (shiftedCharAsciiCode > limit_max) {
-            asciiCodeRepositioning = -alphabet;
-        }
-        // Returns the shifted character
-        return String.fromCharCode(shiftedCharAsciiCode + asciiCodeRepositioning);
 }
 
 
