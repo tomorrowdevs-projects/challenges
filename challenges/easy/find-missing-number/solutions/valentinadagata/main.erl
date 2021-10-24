642b89cfd916c98b8885e560d0ae2bcabecd81a7
Edera <edera.marketinglab@gmail.com>
2021-10-24 18:45:22 +0200

    findMissingNumber function was created to return the result

--- challenges/easy/find-missing-number/solutions/valentinadagata/main.erl
+++ challenges/easy/find-missing-number/solutions/valentinadagata/main.erl
@@ -2,9 +2,11 @@
 -import(lists,[sum/1]).
 -export([start/0]).
 
-start() ->
-  MyList=[230, 222, 220, 224, 229, 221, 225, 223, 228, 226],
+findMissingNumber(MyList) ->
   Total = sum(lists:seq(lists:min(MyList), lists:max(MyList))),
   MissingNum = Total - sum(MyList),
-  io:fwrite("~w~n", [MissingNum]),
   MissingNum.
+
+start() ->
+  MyList=[230, 222, 220, 224, 229, 221, 225, 223, 228, 226],
+  io:fwrite("~w~n", [findMissingNumber(MyList)]).
