c9e3da8dc76adb8fa538aa876cdbc446b6bd60a6
Francesco <cicciodb@hotmail.it>
2021-10-27 16:45:21 +0200

    let replaced by const

--- challenges/easy/caesar-cipher/index.ts
+++ challenges/easy/caesar-cipher/index.ts
@@ -1,21 +1,17 @@
 function encode(word: string, shift: number) {
 
   shift = shift % 26;
-  let ascii: number;
-  let new_code: number;
   
   return word.split("").map(caesar => {
-    new_code = 0;
-    ascii = caesar.charCodeAt(0);
+    const ascii: number = caesar.charCodeAt(0);
+    const new_code: number = ascii + shift;
     if (ascii >= 65 && ascii <= 90) {
-      new_code = ascii + shift;
-      if (new_code < 65) {new_code += 26}
-      if (new_code > 90) {new_code -= 26}
-      return String.fromCharCode(new_code)
+      if (new_code < 65) {return String.fromCharCode(new_code + 26)}
+      if (new_code > 90) {return String.fromCharCode(new_code - 26)}
+      else return String.fromCharCode(new_code)
     } else if (ascii >= 97 && ascii <= 122) {
-      new_code = ascii + shift;
-      if (new_code < 97) {new_code += 26}
-      if (new_code > 122) {new_code -= 26}
+      if (new_code < 97) {return String.fromCharCode(new_code + 26)}
+      if (new_code > 122) {return String.fromCharCode(new_code - 26)}
       return String.fromCharCode(new_code)
     } else return caesar;
   }).join('');
