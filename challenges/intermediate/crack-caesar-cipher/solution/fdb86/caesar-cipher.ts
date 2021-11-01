41a424ec9844e0cab52f721c4abff641cc7c1348
Francesco <cicciodb@hotmail.it>
2021-11-01 19:48:35 +0100

    variables corrected as requested

--- challenges/intermediate/crack-caesar-cipher/solution/fdb86/caesar-cipher.ts
+++ challenges/intermediate/crack-caesar-cipher/solution/fdb86/caesar-cipher.ts
@@ -1,18 +1,25 @@
-export function encode(word: string, shift: number) {
+function encode(word: string, shift: number) {
 
-  shift = shift % 26;
+  const alphabetLength = 26;
+  // reduce the shift factor to real position to shift if the shift factor is more than 26, else the result is the same of starting shift.
+  const moduleShift = shift % alphabetLength;
   
   return word.split("").map(caesar => {
-    const ascii: number = caesar.charCodeAt(0);
-    const new_code: number = ascii + shift;
-    if (ascii >= 65 && ascii <= 90) {
-      if (new_code < 65) {return String.fromCharCode(new_code + 26)}
-      if (new_code > 90) {return String.fromCharCode(new_code - 26)}
+    const ascii = caesar.charCodeAt(0);
+    const new_code = ascii + moduleShift;
+
+    //range of values for upper and lower letters in ASCII code
+    const minimumUpper = 65; const maximumUpper = 90;
+    const minimumLower = 97; const maximumLower = 122;
+
+    if (ascii >= minimumUpper && ascii <= maximumUpper) {
+      if (new_code < minimumUpper) {return String.fromCharCode(new_code + alphabetLength)}
+      if (new_code > maximumUpper) {return String.fromCharCode(new_code - alphabetLength)}
       else return String.fromCharCode(new_code)
-    } else if (ascii >= 97 && ascii <= 122) {
-      if (new_code < 97) {return String.fromCharCode(new_code + 26)}
-      if (new_code > 122) {return String.fromCharCode(new_code - 26)}
-      return String.fromCharCode(new_code)
+    } else if (ascii >= minimumLower && ascii <= maximumLower) {
+      if (new_code < minimumLower) {return String.fromCharCode(new_code + alphabetLength)}
+      if (new_code > maximumLower) {return String.fromCharCode(new_code - alphabetLength)}
+      return String.fromCharCode(new_code)  
     } else return caesar;
   }).join('');
 }
