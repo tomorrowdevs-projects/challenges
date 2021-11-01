149b6fed742f18e5f33c174d3900d7b80b399f31
Francesco <cicciodb@hotmail.it>
2021-11-01 19:49:48 +0100

    variables corrected with more explicative name and deleted constant not used

--- challenges/intermediate/crack-caesar-cipher/solution/fdb86/hack-caesar.ts
+++ challenges/intermediate/crack-caesar-cipher/solution/fdb86/hack-caesar.ts
@@ -1,18 +1,12 @@
-import {encode} from './caesar-cipher'
 import {decode} from './caesar-cipher'
 
-const englishLetterFreq = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97, 'N': 6.75,
-                           'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25, 'L': 4.03, 'C': 2.78,
-                           'U': 2.76, 'M': 2.41, 'W': 2.36, 'F': 2.23, 'G': 2.02, 'Y': 1.97,
-                           'P': 1.93, 'B': 1.29, 'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15,
-                           'Q': 0.10, 'Z': 0.07}
-
-const ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
+//string representing letters in order of statistic use in english alphabet
+const frequencyLetters = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
 
 function getShiftFactor (message: string) {
 
 
-    var letterCount: any = {}
+    let letterCount: any = {}
     const splitted: any = message.toUpperCase().split("")
 
     for (let letter of splitted) {
@@ -25,7 +19,7 @@ function getShiftFactor (message: string) {
     const max = Math.max(...nums);
     const char = Object.keys(letterCount).find(letter => letterCount[letter] === max)!;
     
-    return (char.charCodeAt(0) - ETAOIN.charCodeAt(0))
+    return (char.charCodeAt(0) - frequencyLetters.charCodeAt(0))
 
 }
 
