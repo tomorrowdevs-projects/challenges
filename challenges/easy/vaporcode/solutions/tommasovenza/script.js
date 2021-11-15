58dfa628d40ba66ebecb2e1751c729c168921e7a
Tommaso <tommasovenza@gmail.com>
2021-11-15 20:16:04 +0100

    solved with javascript's reduce method

--- challenges/easy/vaporcode/solutions/tommasovenza/script.js
+++ challenges/easy/vaporcode/solutions/tommasovenza/script.js
@@ -6,13 +6,41 @@
 // While this may seems like a very simple challenge, it will become a bit harder,
 // 'cause you have to solve it by using a language that you've never seen before.
 
+function vaporwaveReduce(string) {
+  const array = []
+  for (let index = 0; index < string.length; index++) {
+    array.push(string[index])
+  }
+  const stringReduce = array.reduce((firstVal, currentVal) => {
+    if (currentVal === " ") {
+      return (firstVal += "")
+    } else {
+      return (firstVal += " " + currentVal + " ")
+    }
+  }, "")
+
+  const result = stringReduce.substr(1, stringReduce.length - 2).toUpperCase()
+
+  return result
+}
+
 function vaporwave(string) {
   let finalString = ""
 
   for (let index = 0; index < string.length; index++) {
-    finalString += string[index].toUpperCase() + " "
+    if (string[index] === " ") {
+      finalString += ""
+    } else {
+      finalString += string[index].toUpperCase() + "  "
+    }
   }
-  return finalString
+  // remove last two empty characters added before
+  const result = finalString.substr(0, finalString.length - 2)
+
+  return result
 }
 
-console.log(vaporwave("I love JavaScript!"))
+const resultVaporwave = vaporwave("I love JavaScript!")
+const resultVaporwaveReduce = vaporwaveReduce("I love php!")
+console.log(resultVaporwave)
+console.log(resultVaporwaveReduce)
