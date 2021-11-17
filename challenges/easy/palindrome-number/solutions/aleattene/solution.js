7139c1411a899e5a2d0d08b65160c65ddd6b9516
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-11-17 12:45:09 +0100

    feat: added js solution (and tests) for challenge palindrome-number

---
+++ challenges/easy/palindrome-number/solutions/aleattene/solution.js
@@ -0,0 +1,32 @@
+/*
+JS solution for challenge: "Palindrome Number"
+To test the solution, type from CLI: npm test (required node.js and jest framework)
+*/
+
+
+function isPalindrome(number) {
+    // From number to String
+    let word = number.toString()
+
+    // Shift indexes (left and right)
+    let i = 0;
+    let j = word.length - 1;
+
+    // Returns false as soon as two different values are found
+    while (i <= j) {
+         if (word[i] !== word[j]) {
+             // The number is not palindrome
+             return false;
+         }
+         i++; j--;
+    }
+    // The number is palindrome
+    return true;
+}
+
+
+
+// Exports for the tests
+module.exports = {
+    isPalindrome,
+}
